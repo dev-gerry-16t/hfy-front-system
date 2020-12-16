d7a63fc7100cffabd630b48222a764fd80e23ca3
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2020-12-16 12:23:37 -0600

    add files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import LoginDemo from "./view/Login/login";
+
+function App() {
+  return (
+    <div className="App">
+      <LoginDemo />
+    </div>
+  );
+}
+
+export default App;
