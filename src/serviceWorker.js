78b3d409c16d51b793c30b79f71c9a82f8c13ace
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2020-12-17 18:49:44 -0600

    workbox v2

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -29,8 +29,19 @@ export function register(config) {
       return;
     }
 
+    self.addEventListener('fetch', (event) => {
+      event.respondWith(
+        caches.match(event.request).then((response) => {
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }),
+      );
+    });
+
     window.addEventListener("load", () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
