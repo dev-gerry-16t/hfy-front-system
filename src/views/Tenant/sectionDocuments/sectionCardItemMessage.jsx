1cc35c9073284e7c31f284620850c9a08a8988a6
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2021-01-15 21:22:34 -0600

    add response message

--- src/views/Tenant/sectionDocuments/sectionCardItemMessage.jsx
+++ src/views/Tenant/sectionDocuments/sectionCardItemMessage.jsx
@@ -14,14 +14,16 @@ const SectionCardItemMessage = () => {
           </div>
           <div className="message-name-send">
             <strong>Patricia Ramirez</strong>
+            <span style={{ fontSize: "12px" }}>Inquilino</span>
             <span>Viernes, 4:18 pm</span>
           </div>
         </div>
         <div className="item-message-2">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie
-          luctus tempor mauris vitae rhoncus, faucibus integer. Diam quis
-          vivamus ut odio aliquam erat odio non. Lacinia tellus risus pretium ut
-          in porttitor nunc. Amet massa ac semper in venenatis vulputate.
+          luctus tempor mauris vitae rhoncus, faucibus integer. Diam quis.
+        </div>
+        <div className="response-message-item">
+          <textarea placeholder="Responder" maxlength="150" />
         </div>
       </div>
     </Item>
