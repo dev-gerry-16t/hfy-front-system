699468d275792aebe37b23f22f34741bb0aef730
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2021-02-15 09:51:11 -0600

    add styles

--- src/utils/constants/globalConstants.js
+++ src/utils/constants/globalConstants.js
@@ -2,7 +2,7 @@ const GLOBAL_CONSTANTS = {
   secretKey: "",
   tokenSecretKey: "",
   ROOT_NAME: "system",
-  VERSION: "Homify v0.0.22",
+  VERSION: "Homify v0.0.23",
   STATUS_API: {
     WARNING: "WARNING",
     SUCCESS: "SUCCESS",
