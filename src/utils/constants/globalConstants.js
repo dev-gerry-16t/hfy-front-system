a96d9ebc0136b6e51cdd695da5697bb2659a833f
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2021-03-02 10:50:33 -0600

    change version

--- src/utils/constants/globalConstants.js
+++ src/utils/constants/globalConstants.js
@@ -2,7 +2,7 @@ const GLOBAL_CONSTANTS = {
   secretKey: "",
   tokenSecretKey: "",
   ROOT_NAME: "system",
-  VERSION: "v0.0.36",
+  VERSION: "v0.0.37",
   STATUS_API: {
     WARNING: "WARNING",
     SUCCESS: "SUCCESS",
