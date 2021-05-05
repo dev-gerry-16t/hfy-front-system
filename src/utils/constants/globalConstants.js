eb786e3ebee1c3814c895aef24ce51feaf6e78b1
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2021-05-05 13:53:54 -0500

    stripe + payment info + view file

--- src/utils/constants/globalConstants.js
+++ src/utils/constants/globalConstants.js
@@ -2,7 +2,7 @@ const GLOBAL_CONSTANTS = {
   secretKey: "",
   tokenSecretKey: "",
   ROOT_NAME: "system",
-  VERSION: "v0.0.98",
+  VERSION: "v0.0.99",
   STATUS_API: {
     WARNING: "WARNING",
     SUCCESS: "SUCCESS",
