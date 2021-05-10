ad487a4f3c1f6a27d82e40de4dabf4883f86ed32
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2021-05-10 12:25:31 -0500

    flow payment incidence

--- src/utils/constants/globalConstants.js
+++ src/utils/constants/globalConstants.js
@@ -2,7 +2,7 @@ const GLOBAL_CONSTANTS = {
   secretKey: "",
   tokenSecretKey: "",
   ROOT_NAME: "system",
-  VERSION: "v0.0.101",
+  VERSION: "v0.0.102",
   STATUS_API: {
     WARNING: "WARNING",
     SUCCESS: "SUCCESS",
