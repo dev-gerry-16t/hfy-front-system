98c538728d69f9455aea2a7c699eed8015460418
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2021-06-11 16:24:55 -0500

    delete eslint

--- src/utils/actions/actions.js
+++ src/utils/actions/actions.js
@@ -4744,6 +4744,48 @@ const callUpdateRequestAdvancePym =
     }
   };
 
+const callUpdateProspectInvitation =
+  (data, id) => async (dispatch, getState) => {
+    const state = getState();
+    const { dataProfile } = state;
+    HEADER.Authorization = "Bearer " + dataProfile.dataProfile.token;
+    try {
+      const config = { headers: { ...HEADER } };
+      const response = await RequesterAxios.put(
+        API_CONSTANTS.UPDATE_PROSPECT_INVITATION + id,
+        data,
+        config
+      );
+      const responseResultStatus =
+        isNil(response) === false && isNil(response.status) === false
+          ? response.status
+          : null;
+      const responseResultMessage =
+        isNil(response) === false &&
+        isNil(response.data) === false &&
+        isNil(response.data.response) === false &&
+        isNil(response.data.response.message) === false
+          ? response.data.response.message
+          : null;
+      const responseResultData =
+        isNil(response) === false && isNil(response.data) === false
+          ? response.data
+          : null;
+      if (
+        isNil(responseResultStatus) === false &&
+        responseResultStatus === 200
+      ) {
+        return responseResultData;
+      } else {
+        throw isNil(responseResultMessage) === false
+          ? responseResultMessage
+          : null;
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
+
 const callGetAuditReferences = (data) => async (dispatch, getState) => {
   const state = getState();
   const { dataProfile } = state;
@@ -4985,4 +5027,5 @@ export {
   callUpdateInvitation,
   callGetPropertyCoincidences,
   callValidateTypeFormProperties,
+  callUpdateProspectInvitation,
 };
--- src/utils/constants/globalConstants.js
+++ src/utils/constants/globalConstants.js
@@ -2,7 +2,7 @@ const GLOBAL_CONSTANTS = {
   secretKey: "",
   tokenSecretKey: "",
   ROOT_NAME: "system",
-  VERSION: "v0.0.108",
+  VERSION: "v0.0.109",
   STATUS_API: {
     WARNING: "WARNING",
     SUCCESS: "SUCCESS",
