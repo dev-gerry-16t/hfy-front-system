11dba267493d400f39bb21a44debab3f1abad083
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2021-06-16 20:08:42 -0500

    add signature 4

--- src/utils/constants/globalConstants.js
+++ src/utils/constants/globalConstants.js
@@ -2,7 +2,7 @@ const GLOBAL_CONSTANTS = {
   secretKey: "",
   tokenSecretKey: "",
   ROOT_NAME: "system",
-  VERSION: "v0.0.113",
+  VERSION: "v0.0.114",
   STATUS_API: {
     WARNING: "WARNING",
     SUCCESS: "SUCCESS",
