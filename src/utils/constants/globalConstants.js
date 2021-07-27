fdf4328eea765ecc65f157a3421e96214bdf8d7a
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2021-07-27 17:00:45 -0500

    terms and conditions

--- src/utils/constants/globalConstants.js
+++ src/utils/constants/globalConstants.js
@@ -2,7 +2,7 @@ const GLOBAL_CONSTANTS = {
   secretKey: "",
   tokenSecretKey: "",
   ROOT_NAME: "system",
-  VERSION: "v0.0.141",
+  VERSION: "v0.0.142",
   STATUS_API: {
     WARNING: "WARNING",
     SUCCESS: "SUCCESS",
