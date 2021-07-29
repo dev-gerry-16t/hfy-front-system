7aceaf98e7c7e1731bf931f862b3c4e745c8b323
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2021-07-29 10:46:25 -0500

    fix serviceworker

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-serviceWorkerRegistration.register(swConfig);
+serviceWorkerRegistration.unregister(swConfig);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
