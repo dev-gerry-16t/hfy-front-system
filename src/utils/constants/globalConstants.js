5d07a814fc903907813c80310edf055769ddc43d
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2021-11-26 14:55:43 -0600

    add images empty

--- src/utils/constants/globalConstants.js
+++ src/utils/constants/globalConstants.js
@@ -2,7 +2,7 @@ const GLOBAL_CONSTANTS = {
   secretKey: "",
   tokenSecretKey: "",
   ROOT_NAME: "system",
-  VERSION: "v0.1.171",
+  VERSION: "v0.1.172",
   STATUS_API: {
     WARNING: "WARNING",
     SUCCESS: "SUCCESS",
