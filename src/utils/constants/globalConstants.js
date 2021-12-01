492cccec4d5bd1e781c0ce7a0e0005517cb710c3
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2021-12-01 12:55:22 -0600

    add validation isOwner validations

--- src/utils/constants/globalConstants.js
+++ src/utils/constants/globalConstants.js
@@ -2,7 +2,7 @@ const GLOBAL_CONSTANTS = {
   secretKey: "",
   tokenSecretKey: "",
   ROOT_NAME: "system",
-  VERSION: "v0.1.181",
+  VERSION: "v0.1.182",
   STATUS_API: {
     WARNING: "WARNING",
     SUCCESS: "SUCCESS",
--- src/views/Properties/sectionsDetail/sectionAgents.jsx
+++ src/views/Properties/sectionsDetail/sectionAgents.jsx
@@ -34,11 +34,12 @@ const GeneralCard = styled.div`
     }
   }
   .content-cards {
-    min-height: 30em;
     padding: 2em 2em;
     display: flex;
     flex-direction: column;
     gap: 1.5em;
+    max-height: 30em;
+    overflow-y: scroll;
   }
 `;
 
@@ -46,7 +47,6 @@ const Card = styled.div`
   background: #ffffff;
   box-shadow: 0px 6px 22px 12px rgba(205, 213, 219, 0.6);
   border-radius: 4px;
-  min-height: 6em;
   .card-document {
     padding: 1em;
     .top-info {
