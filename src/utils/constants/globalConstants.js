0eb46b086f8d0695e1d8975d10c986b5177f77a2
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2021-12-06 17:39:08 -0600

    add data aval

--- src/utils/constants/globalConstants.js
+++ src/utils/constants/globalConstants.js
@@ -2,7 +2,7 @@ const GLOBAL_CONSTANTS = {
   secretKey: "",
   tokenSecretKey: "",
   ROOT_NAME: "system",
-  VERSION: "v0.1.192",
+  VERSION: "v0.1.193",
   STATUS_API: {
     WARNING: "WARNING",
     SUCCESS: "SUCCESS",
