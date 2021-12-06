5d747e20c5a10edc04ab699a68d7bec870bd58c0
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2021-12-06 12:57:37 -0600

    add card contact

--- src/utils/constants/globalConstants.js
+++ src/utils/constants/globalConstants.js
@@ -2,7 +2,7 @@ const GLOBAL_CONSTANTS = {
   secretKey: "",
   tokenSecretKey: "",
   ROOT_NAME: "system",
-  VERSION: "v0.1.191",
+  VERSION: "v0.1.192",
   STATUS_API: {
     WARNING: "WARNING",
     SUCCESS: "SUCCESS",
