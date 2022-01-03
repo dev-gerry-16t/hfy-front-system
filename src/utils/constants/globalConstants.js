fe79d09f56be3e1b8e69bb5c0258ad8ec6731309
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2022-01-02 21:31:32 -0600

    issue save data Property

--- src/utils/constants/globalConstants.js
+++ src/utils/constants/globalConstants.js
@@ -2,7 +2,7 @@ const GLOBAL_CONSTANTS = {
   secretKey: "",
   tokenSecretKey: "",
   ROOT_NAME: "system",
-  VERSION: "v0.1.219",
+  VERSION: "v0.1.220",
   STATUS_API: {
     WARNING: "WARNING",
     SUCCESS: "SUCCESS",
