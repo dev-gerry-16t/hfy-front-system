5f5b555fbea5bdff486600e48578be0faf196e6d
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2022-01-07 19:53:40 -0600

    add link share

--- src/views/Properties/detailPropertyUsers.jsx
+++ src/views/Properties/detailPropertyUsers.jsx
@@ -487,6 +487,29 @@ const DetailPropertyUsers = (props) => {
                   overlay={
                     <Menu>
                       <Menu.Item>
+                        <span
+                          onClick={() => {
+                            navigator
+                              .share({
+                                title: dataDetail.title,
+                                text: dataDetail.description,
+                                url: `${window.location.origin}/property/${dataDetail.identifier}`,
+                              })
+                              .then(() =>
+                                frontFunctions.showMessageStatusApi(
+                                  "¡Excelente, compartiste la propiedad!",
+                                  GLOBAL_CONSTANTS.STATUS_API.SUCCESS
+                                )
+                              )
+                              .catch((error) =>
+                                console.log("Error in sharing", error)
+                              );
+                          }}
+                        >
+                          Compartir
+                        </span>
+                      </Menu.Item>
+                      <Menu.Item>
                         <a
                           target="_blank"
                           href={`https://wa.me/?text=Te+invito+a+que+veas+esta+propiedad%0a${window.location.origin}/property/${dataDetail.identifier}`}
