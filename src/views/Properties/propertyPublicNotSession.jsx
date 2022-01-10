f83e027f458211a492274c01401a50cf545b8551
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2022-01-10 13:31:57 -0600

    add metatags

--- src/views/Properties/propertyPublicNotSession.jsx
+++ src/views/Properties/propertyPublicNotSession.jsx
@@ -98,6 +98,11 @@ const DetailPropertyUsers = (props) => {
         <meta name="description" content={dataDetail.description} />
         <meta property="og:title" content={dataDetail.title} />
         <meta property="og:image" content={dataDetail.documentMainPic} />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content={`${window.location.origin}/property/${dataDetail.identifier}`}
+        />
       </MetaTags>
       <div className="ant-layout site-layout">
         <Content>
