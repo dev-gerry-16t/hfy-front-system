a44aa613b99b0120eba89dae021149ae17b6c4f4
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2022-01-20 22:06:39 -0600

    save image sqarer

--- src/views/Properties/sectionsDetail/sectionApplicants.jsx
+++ src/views/Properties/sectionsDetail/sectionApplicants.jsx
@@ -13,11 +13,45 @@ import FrontFunctions from "../../../utils/actions/frontFunctions";
 import { callGlobalActionApi } from "../../../utils/actions/actions";
 import ComponentLoadSection from "../../../components/componentLoadSection";
 import { GeneralCard } from "../constants/styleConstants";
+import { ReactComponent as ArrowUp2 } from "../../../assets/iconSvg/svgFile/arrowUp2.svg";
+import { ReactComponent as ArrowDown2 } from "../../../assets/iconSvg/svgFile/arrowDown2.svg";
+import { useEffect } from "react";
+
+const CardStatus = styled.div`
+  position: relative;
+  background: #ff0606;
+  width: 16px;
+  border-radius: 4px 0px 0px 4px;
+  height: 150px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  box-shadow: 0px 6px 22px 12px rgba(205, 213, 219, 0.6);
+  .status-prospect {
+    position: absolute;
+    transform: rotate(-90deg);
+    width: 150px;
+    left: -68px;
+    display: flex;
+    justify-content: center;
+    span {
+      font-weight: 700;
+      color: #fff;
+      font-size: 10px;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+  }
+`;
 
 const Card = styled.div`
   background: #ffffff;
   box-shadow: 0px 6px 22px 12px rgba(205, 213, 219, 0.6);
   border-radius: 4px;
+  .all-content-pre-info {
+    display: flex;
+  }
   .card-document {
     padding: 1em;
     .top-info {
@@ -50,7 +84,11 @@ const Card = styled.div`
   }
 
   .card-user {
-    padding: 1em;
+    padding: 1em 1em 0px 1em;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 100%;
     .top-info {
       display: flex;
       .icon-info {
@@ -90,17 +128,33 @@ const Card = styled.div`
         flex-direction: column;
         h3 {
           margin: 0px;
+          font-weight: 800;
+        }
+        u {
+          color: #4e4b66;
+          margin-top: 5px;
         }
         span {
-          color: var(--color-primary);
           overflow: hidden;
           text-overflow: ellipsis;
+          margin-top: 5px;
+          color: #000;
+          font-weight: 500;
         }
       }
     }
     .button-action {
       display: flex;
-      justify-content: flex-end;
+      justify-content: center;
+    }
+    .toggle-card {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      padding: 10px 0px;
+      svg {
+        cursor: pointer;
+      }
     }
   }
 `;
@@ -130,6 +184,26 @@ const EmptyData = styled.div`
   }
 `;
 
+const ContentDetail = styled.div`
+  max-height: ${(props) => (props.visible === true ? "500px" : "0px")};
+  overflow: hidden;
+  font-size: 12px;
+  padding: ${(props) => (props.visible === true ? "15px" : "0px")};
+  border-top: 0.3px solid #bdbdbd;
+  transition: all 0.4s ease-in-out;
+  .title-desc {
+    color: var(--color-primary);
+  }
+  .value-desc {
+    color: #000;
+    font-weight: 500;
+  }
+  .button-action {
+    display: flex;
+    justify-content: center;
+  }
+`;
+
 const SectionApplicants = (props) => {
   const { idUserType, callGlobalActionApi, dataProfile } = props;
   const dataContexProperty = useContext(ContextProperty);
@@ -140,6 +214,7 @@ const SectionApplicants = (props) => {
     onCloseComponent,
   } = dataContexProperty;
   const [isLoadApi, setIsLoadApi] = useState(false);
+  const [toggleCard, setToggleCard] = useState(false);
   const { applicants, idApartment, idProperty, canInviteTenant } = dataDetail;
 
   const applicantsArray =
@@ -216,6 +291,16 @@ const SectionApplicants = (props) => {
     }
   };
 
+  useEffect(() => {
+    if (isEmpty(applicants) === false) {
+      const objectState = {};
+      applicantsArray.forEach((element) => {
+        objectState[element.prospectNo] = false;
+      });
+      setToggleCard(objectState);
+    }
+  }, [applicants]);
+
   return (
     <GeneralCard id="section-applicants">
       <ComponentAddCandidate
@@ -257,43 +342,188 @@ const SectionApplicants = (props) => {
                     isNil(row.idCustomer) === false ? row.idCustomer : ix
                   }`}
                 >
-                  <div className="card-user">
-                    <div className="top-info">
-                      <div className="icon-info">
-                        <IconTenant size="100%" color="#4E4B66" />
-                        {isNil(row.score) === false && (
-                          <div className="score">
-                            <span>Score</span>
-                            <strong>{row.score}</strong>
+                  <div className="all-content-pre-info">
+                    <CardStatus>
+                      <div className="status-prospect">
+                        <span>{row.applicantStatus}</span>
+                      </div>
+                    </CardStatus>
+                    <div className="card-user">
+                      <div className="top-info">
+                        <div className="icon-info">
+                          <IconTenant size="100%" color="#4E4B66" />
+                          {isNil(row.score) === false && (
+                            <div className="score">
+                              <span>Score</span>
+                              <strong>{row.score}</strong>
+                            </div>
+                          )}
+                        </div>
+                        <div className="name-info">
+                          <h3>{row.fullName}</h3>
+                          <u>{row.emailAddress}</u>
+                          <span>{row.phoneNumber}</span>
+                        </div>
+                      </div>
+                      {row.canProcessInvitation === false &&
+                        row.canBeProcessed === true &&
+                        toggleCard[row.prospectNo] === false && (
+                          <div
+                            className="button-action"
+                            id={`user-applicant-canBeProcessed-${
+                              isNil(row.idCustomer) === false
+                                ? row.idCustomer
+                                : ix
+                            }`}
+                          >
+                            <Popconfirm
+                              placement="left"
+                              title="¿Estás seguro de rechazar a este prospecto?"
+                              onConfirm={async () => {
+                                try {
+                                  setIsLoadApi(true);
+                                  await handlerCallSetApplicant({
+                                    idCustomer: row.idCustomer,
+                                    idInvitation: row.idInvitation,
+                                    isAccepted: false,
+                                  });
+                                  setIsLoadApi(false);
+                                } catch (error) {
+                                  setIsLoadApi(false);
+                                  throw error;
+                                }
+                              }}
+                              okText="Si"
+                              cancelText="No"
+                            >
+                              <ButtonDocument onClick={() => {}}>
+                                Rechazar
+                              </ButtonDocument>
+                            </Popconfirm>
+                            <ButtonDocument
+                              onClick={async () => {
+                                try {
+                                  setIsLoadApi(true);
+                                  await handlerCallSetApplicant({
+                                    idCustomer: row.idCustomer,
+                                    idInvitation: row.idInvitation,
+                                    isAccepted: true,
+                                  });
+                                  setIsLoadApi(false);
+                                } catch (error) {
+                                  setIsLoadApi(false);
+                                  throw error;
+                                }
+                              }}
+                            >
+                              Aceptar
+                            </ButtonDocument>
                           </div>
                         )}
-                      </div>
-                      <div className="name-info">
-                        <h3>{row.fullName}</h3>
-                        <span>{row.emailAddress}</span>
-                        <span>{row.phoneNumber}</span>
-                        {isNil(row.origin) === false && (
-                          <span
-                            style={{
-                              fontStyle: "italic",
-                            }}
+                      {row.canProcessInvitation === true &&
+                        toggleCard[row.prospectNo] === false && (
+                          <div
+                            className="button-action"
+                            id={`user-applicant-canProcessInvitation-${
+                              isNil(row.idCustomer) === false
+                                ? row.idCustomer
+                                : ix
+                            }`}
                           >
-                            {" "}
-                            Enviado por: {row.origin}
-                          </span>
+                            <Popconfirm
+                              placement="left"
+                              title="¿Estás seguro de rechazar a este prospecto?"
+                              onConfirm={async () => {
+                                try {
+                                  setIsLoadApi(true);
+                                  await handlerCallSetApplicant({
+                                    idCustomer: row.idCustomer,
+                                    idInvitation: row.idInvitation,
+                                    deleteInvitation: true,
+                                  });
+                                  setIsLoadApi(false);
+                                } catch (error) {
+                                  setIsLoadApi(false);
+                                  throw error;
+                                }
+                              }}
+                              okText="Si"
+                              cancelText="No"
+                            >
+                              <ButtonDocument onClick={() => {}}>
+                                Eliminar
+                              </ButtonDocument>
+                            </Popconfirm>
+                            <ButtonDocument
+                              onClick={async () => {
+                                try {
+                                  setIsLoadApi(true);
+                                  await handlerCallSetApplicant({
+                                    idCustomer: row.idCustomer,
+                                    idInvitation: row.idInvitation,
+                                    resendInvitation: true,
+                                  });
+                                  setIsLoadApi(false);
+                                } catch (error) {
+                                  setIsLoadApi(false);
+                                  throw error;
+                                }
+                              }}
+                            >
+                              Reenviar
+                            </ButtonDocument>
+                          </div>
                         )}
-
-                        <strong
-                          style={{
-                            color: "#4E4B66",
-                          }}
-                        >
-                          {row.applicantStatus}
-                        </strong>
+                      <div className="toggle-card">
+                        {toggleCard[row.prospectNo] === false ? (
+                          <ArrowDown2
+                            stroke="#200E32"
+                            width="1.5em"
+                            onClick={() => {
+                              setToggleCard({
+                                ...toggleCard,
+                                [row.prospectNo]: !toggleCard[row.prospectNo],
+                              });
+                            }}
+                          />
+                        ) : (
+                          <ArrowUp2
+                            stroke="#200E32"
+                            width="1.5em"
+                            onClick={() => {
+                              setToggleCard({
+                                ...toggleCard,
+                                [row.prospectNo]: !toggleCard[row.prospectNo],
+                              });
+                            }}
+                          />
+                        )}
                       </div>
                     </div>
+                  </div>
+                  <ContentDetail visible={toggleCard[row.prospectNo]}>
+                    <p>
+                      <span className="title-desc">Invitado por:</span>
+                      <br />
+                      <span className="value-desc">{row.origin}</span>
+                    </p>
+                    <p>
+                      <span className="title-desc">Descripción:</span>
+                      <br />
+                      <span className="value-desc">
+                        {row.applicantStatusDes}
+                      </span>
+                    </p>
+                    <p>
+                      <span className="title-desc">Último cambio:</span>
+                      <br />
+                      <span className="value-desc">
+                        {row.applicantStatusDesDate}
+                      </span>
+                    </p>
                     {row.canProcessInvitation === false &&
-                      row.canBeProcessed === true && (
+                      row.canBeProcessed === true &&
+                      toggleCard[row.prospectNo] === true && (
                         <div
                           className="button-action"
                           id={`user-applicant-canBeProcessed-${
@@ -346,58 +576,61 @@ const SectionApplicants = (props) => {
                           </ButtonDocument>
                         </div>
                       )}
-                    {row.canProcessInvitation === true && (
-                      <div
-                        className="button-action"
-                        id={`user-applicant-canProcessInvitation-${
-                          isNil(row.idCustomer) === false ? row.idCustomer : ix
-                        }`}
-                      >
-                        <Popconfirm
-                          placement="left"
-                          title="¿Estás seguro de rechazar a este prospecto?"
-                          onConfirm={async () => {
-                            try {
-                              setIsLoadApi(true);
-                              await handlerCallSetApplicant({
-                                idCustomer: row.idCustomer,
-                                idInvitation: row.idInvitation,
-                                deleteInvitation: true,
-                              });
-                              setIsLoadApi(false);
-                            } catch (error) {
-                              setIsLoadApi(false);
-                              throw error;
-                            }
-                          }}
-                          okText="Si"
-                          cancelText="No"
+                    {row.canProcessInvitation === true &&
+                      toggleCard[row.prospectNo] === true && (
+                        <div
+                          className="button-action"
+                          id={`user-applicant-canProcessInvitation-${
+                            isNil(row.idCustomer) === false
+                              ? row.idCustomer
+                              : ix
+                          }`}
                         >
-                          <ButtonDocument onClick={() => {}}>
-                            Eliminar
+                          <Popconfirm
+                            placement="left"
+                            title="¿Estás seguro de rechazar a este prospecto?"
+                            onConfirm={async () => {
+                              try {
+                                setIsLoadApi(true);
+                                await handlerCallSetApplicant({
+                                  idCustomer: row.idCustomer,
+                                  idInvitation: row.idInvitation,
+                                  deleteInvitation: true,
+                                });
+                                setIsLoadApi(false);
+                              } catch (error) {
+                                setIsLoadApi(false);
+                                throw error;
+                              }
+                            }}
+                            okText="Si"
+                            cancelText="No"
+                          >
+                            <ButtonDocument onClick={() => {}}>
+                              Eliminar
+                            </ButtonDocument>
+                          </Popconfirm>
+                          <ButtonDocument
+                            onClick={async () => {
+                              try {
+                                setIsLoadApi(true);
+                                await handlerCallSetApplicant({
+                                  idCustomer: row.idCustomer,
+                                  idInvitation: row.idInvitation,
+                                  resendInvitation: true,
+                                });
+                                setIsLoadApi(false);
+                              } catch (error) {
+                                setIsLoadApi(false);
+                                throw error;
+                              }
+                            }}
+                          >
+                            Reenviar
                           </ButtonDocument>
-                        </Popconfirm>
-                        <ButtonDocument
-                          onClick={async () => {
-                            try {
-                              setIsLoadApi(true);
-                              await handlerCallSetApplicant({
-                                idCustomer: row.idCustomer,
-                                idInvitation: row.idInvitation,
-                                resendInvitation: true,
-                              });
-                              setIsLoadApi(false);
-                            } catch (error) {
-                              setIsLoadApi(false);
-                              throw error;
-                            }
-                          }}
-                        >
-                          Reenviar
-                        </ButtonDocument>
-                      </div>
-                    )}
-                  </div>
+                        </div>
+                      )}
+                  </ContentDetail>
                 </Card>
               );
             })}
